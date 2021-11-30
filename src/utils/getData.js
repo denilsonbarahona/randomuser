b04e35d1afd6bbaa1fa86c024f7f547ebc6838d4
Denilson Barahona <91sbarahona@gmail.com>
2021-11-30 14:37:17 -0600

    process env fiexd

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,6 +1,6 @@
 /* const API = 'https://randomuser.me/api/';*/
 
-const API = process.env.API;
+const API = process.env.API; 
 
 const getData = async (id) => {
   const apiURl = id ? `${API}${id}` : API;
