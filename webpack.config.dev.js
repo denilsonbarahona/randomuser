7d7ec7adcda03faf617cf0af5bc732901b370414
Denilson Barahona <91sbarahona@gmail.com>
2021-11-30 21:33:04 -0600

    finalización del webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,8 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
- const Dotenv = require("dotenv-webpack");
-
+const Dotenv = require("dotenv-webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   entry: './src/index.js',
@@ -13,6 +13,7 @@ module.exports = {
     assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
   mode: "development",
+  devtool: "source-map",
   watch: true,
   resolve: {
     extensions: ['.js'],
@@ -78,6 +79,13 @@ module.exports = {
       ]
     }),
     new Dotenv(),
+    new BundleAnalyzerPlugin()
   ],
+  devServer: {
+    /* contentBase: path.join(__dirname, "dist"), */
+    compress: true,
+    historyApiFallback: true,
+    port: 3006,
+  }
 
 }
